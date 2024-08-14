5628af53436ad1a6420ceeb8acc242df8dca2888
Rajesh Kumar <rajakumarlohano@gmail.com>
2024-08-14 13:57:29 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -5,5 +5,75 @@ from torch.utils.data import Dataset
 from skimage.transform import resize
 
 class BratsDataset(Dataset):
-    # [Include the full definition as in the original code]
-    pass
+    def __init__(self, df: pd.DataFrame, phase: str = "test", is_resize: bool = False):
+        self.df = df
+        self.phase = phase
+        self.augmentations = None  # Placeholder: Define actual augmentations if needed
+        self.data_types = ['_flair.nii.gz', '_t1.nii.gz', '_t1ce.nii.gz', '_t2.nii.gz']
+        self.is_resize = is_resize
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, idx):
+        id_ = self.df.loc[idx, 'Brats20ID']
+        root_path = self.df.loc[self.df['Brats20ID'] == id_]['path'].values[0]
+        images = []
+        for data_type in self.data_types:
+            img_path = os.path.join(root_path, id_ + data_type)
+            img = self.load_img(img_path)
+            if self.is_resize:
+                img = self.resize(img)
+            img = self.normalize(img)
+            images.append(img)
+        img = np.stack(images)
+        img = np.moveaxis(img, (0, 1, 2, 3), (0, 3, 2, 1))
+
+        if self.phase != "test":
+            mask_path = os.path.join(root_path, id_ + "_seg.nii.gz")
+            mask = self.load_img(mask_path)
+            if self.is_resize:
+                mask = self.resize(mask)
+                mask = np.clip(mask.astype(np.uint8), 0, 1).astype(np.float32)
+                mask = np.clip(mask, 0, 1)
+            mask = self.preprocess_mask_labels(mask)
+            if self.augmentations:
+                augmented = self.augmentations(image=img.astype(np.float32), mask=mask.astype(np.float32))
+                img = augmented['image']
+                mask = augmented['mask']
+            return {"Id": id_, "image": img, "mask": mask}
+
+        return {"Id": id_, "image": img}
+
+    def load_img(self, file_path):
+        data = nib.load(file_path)
+        data = np.asarray(data.dataobj)
+        return data
+
+    def normalize(self, data: np.ndarray):
+        data_min = np.min(data)
+        return (data - data_min) / (np.max(data) - arr_min)
+
+    def resize(self, data: np.ndarray):
+        data = resize(data, (78, 120, 120), preserve_range=True)
+        return data
+
+    def preprocess_mask_labels(self, mask: np.ndarray):
+        mask_WT = mask.copy()
+        mask_WT[mask_WT == 1] = 1
+        mask_WT[mask_WT == 2] = 1
+        mask_WT[mask_WT == 4] = 1
+
+        mask_TC = mask.copy()
+        mask_TC[mask_TC == 1] = 1
+        mask_TC[mask_TC == 2] = 0
+        mask_TC[mask_TC == 4] = 1
+
+        mask_ET = mask.copy()
+        mask_ET[mask_ET == 1] = 0
+        mask_ET[mask_ET == 2] = 0
+        mask_ET[mask_ET == 4] = 1
+
+        mask = np.stack([mask_WT, mask_TC, mask_ET])
+        mask = np.moveaxis(mask, (0, 1, 2, 3), (0, 3, 2, 1))
+        return mask
