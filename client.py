ff89032ff82c54b310c992b4fe4269b623e97015
Rajesh Kumar <rajakumarlohano@gmail.com>
2024-08-14 13:55:28 +0800

    Create client.py

---
+++ client.py
@@ -0,0 +1,9 @@
+import torch
+import json
+from torch.optim import Adam
+import torch.nn as nn
+from ipfs_blockchain import add_to_ipfs, globalSC
+
+class ClientHospital:
+    # [Include the full definition as in the original code]
+    pass
