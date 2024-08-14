85cb60d269c2d65eeb7f0bd9a85098f24321e674
Rajesh Kumar <rajakumarlohano@gmail.com>
2024-08-14 13:52:48 +0800

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+import nibabel as nib
+from torch.utils.data import Dataset
+from skimage.transform import resize
+
+class BratsDataset(Dataset):
+    # [Include the full definition as in the original code]
+    pass
